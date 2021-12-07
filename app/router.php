e7d0eb42e81e971e46ccf2c22b15aab81f56aa07
Dungbv <dugnbv2201@gmail.com>
2021-12-07 13:31:25 +0700

    handle param for router

--- app/router.php
+++ app/router.php
@@ -1,11 +1,32 @@
 <?php
 
 function resolve($routes){
-    foreach ($routes as $key => $route){
-        if($_SERVER['REQUEST_METHOD'] === $route['method'] && $_SERVER['REQUEST_URI'] === $route['url']){
+    $notFound = false;
+    foreach ($routes as $route){
+        $arrayUrl = explode('/',$route['url']);
+        unset($arrayUrl[0]);
+        $arrayUri = explode('/',$_SERVER['REQUEST_URI']);
+        unset($arrayUri[0]);
+        if($_SERVER['REQUEST_METHOD'] === $route['method'] && (count($arrayUrl)) === count($arrayUri)){
+            $params = [];
+            foreach ($arrayUrl as $key => $value){
+                $uri =  $arrayUri[$key];
+                if(strpos($arrayUrl[$key],":") === 0 && $uri){
+                    array_push($params,$uri);
+                    continue;
+                }
+                if($value  !== $uri){
+                    $notFound = true;
+                    break;
+                }
+            }
+            if($notFound){
+                break;
+            }
+
             try{
                 require_once dirname(__DIR__).$route['handler'];
-                return call_user_func($route['action']);
+                return call_user_func($route['action'], ...$params);
             }catch (ErrorException $exception){
                 dd($exception);
             }
